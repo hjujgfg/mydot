4386036b5002bf1989df36c73339101b719265d2
hjujgfg <ctapblixpbl4@gmail.com>
2026-03-14 16:15:00 +0000

    some changes to neovom for neotree and small change for the waybar theme

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,6 @@
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.opt.confirm = true
+-- Disable netrw completely at the core level
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,13 +1,1 @@
-return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        use_libuv_file_watcher = true, -- This makes updates "instant"
-        follow_current_file = {
-          enabled = true, -- Focuses the file in the tree when you switch buffers
-        },
-      },
-    },
-  },
-}
+return {}
